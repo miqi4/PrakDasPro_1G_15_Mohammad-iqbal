5444bc865d440dd82b42b744ce7677c4c2cb9447
miqi4 <iqbalop098@gmail.com>
2024-09-12 07:34:25 +0700

    perubahan to identitas

--- P2_Percobaan3_pertanyaan1_15.java
+++ P2_Percobaan3_pertanyaan1_15.java
@@ -1,31 +1,14 @@
 public class P2_Percobaan3_pertanyaan1_15 {
 
     public static void main(String[] args) {
-    System.out.println("----------------------------");
-    System.out.println("--KARTU RENCANA STUDI(KRS)--");
-    System.out.println("----------------------------");
-    System.out.println("NIM      = 244107020105");
-    System.out.println("Nama     = Mohammad iqbal");
-    System.out.println("kelas    = 1G");
-    System.out.println("Prodi    = D-IV Teknik Informatika");
-    System.out.println("Tahun Akademik : 2024/2025 Ganjil");
-    System.out.println("Kelas          : 1G");
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println("| KODE MK    |  MATA KULIAH                            | SKS | JAM |");
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println("| RTI241001  | Pancasila                               |  2  |  2  |");
-    System.out.println("| RTI241002  | Konsep Teknologi Informasi              |  2  |  4  |");
-    System.out.println("| RTI241003  | Critical Thinking dan Problem Solving   |  2  |  4  |");
-    System.out.println("| RTI241004  | Matematika Dasar                        |  3  |  6  |");
-    System.out.println("| RTI241005  | Rekayasa Perangkat Lunak                |  2  |  4  |");
-    System.out.println("| RTI241006  | Dasar Pemrograman                       |  2  |  4  |");
-    System.out.println("| RTI241007  | Praktikum Dasar Pemrograman             |  3  |  6  |");
-    System.out.println("| RTI241008  | Keselamatan dan Kesehatan Kerja         |  2  |  4  |");
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println("|            | JUMLAH                                  |  18 |  34 |");
-    System.out.println("--------------------------------------------------------------------");
-    System.out.println("IP Semester Lalu : 0.00");
-    System.out.println("IP Komulatif     : 0.00");
+        System.out.println("-----------BIODATA------------");
+        System.out.println("Nama     = Mohammad iqbal");
+        System.out.println("NIM      = 244107020105");
+        System.out.println("kelas    = 1G");
+        System.out.println("TTL      = Probolinggo, 4 Desember 2004");
+        System.out.println("Prodi    = D-IV Teknik Informatika");
+        System.out.println("No HP    = 082146852790");
+        System.out.println("Hobi     = Bermain Game");
     
 
 
