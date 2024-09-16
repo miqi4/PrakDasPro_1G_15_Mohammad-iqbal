33919c128b72cb9b2234c3122d3d080361029c7d
miqi4 <iqbalop098@gmail.com>
2024-09-16 10:26:59 +0700

    P3_Percobaan1_PertanyaanNo5

---
+++ siakad15.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class siakad15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama, nim, kelas;
+        byte Absen;
+        double NilaiKuis, NilaiUjian, NilaiTugas, Nilai-Akhir;
+        
+        System.out.print("masukkan nama : ");
+        nama = sc.nextLine();
+        System.out.print("masukkan nim  : ");
+        nim = sc.nextLine();
+        System.out.print("masukkan kelas: ");
+        kelas = sc.nextLine();
+        System.out.print("masukkan absen: ");
+        Absen = sc.nextByte();
+        System.out.print("Nilai Kuis  : ");
+        NilaiKuis = sc.nextDouble();
+        System.out.print("Nilai Tugas : ");
+        NilaiTugas = sc.nextDouble();
+        System.out.print("Nilai Ujian : ");
+        NilaiUjian = sc.nextDouble();
+        Nilai-Akhir = (NilaiKuis + NilaiTugas + NilaiUjian) / 3;
+
+        System.out.println("Mahasiswa dengan Nama " +nama+ " NIM " +nim+" Kelas " +kelas+ " Nomor Absen "+Absen);
+        System.out.println("Nilai-Akhir :"+Nilai-Akhir);
+
+
+
+
+        
+    }
+}
