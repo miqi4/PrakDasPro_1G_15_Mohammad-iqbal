975db413cd0b68a7591e103961116b929bc339df
miqi4 <iqbalop098@gmail.com>
2024-09-17 16:30:23 +0700

    P3_Percobaan2

---
+++ src/pertemuan3/kafe15.java
@@ -0,0 +1,36 @@
+package pertemuan3;
+
+        import java.util.Scanner;
+        
+        public class kafe15 {
+            public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+                boolean keanggotaan;
+                int jmlKopi, jmlTeh, jmlroti;
+                double hargakopi = 12000, hargaTeh = 7000, hargaRoti = 20000, totalharga, nominalBayar;
+                float diskon = 10/100f;
+        
+                System.out.print("masukkan keanggotaan (true/false) :");
+                keanggotaan = input.nextBoolean();
+                System.out.print("masukkan jumlah pembelian kopi :");
+                jmlKopi = input.nextInt();
+                System.out.print("masukkan jumlah pembelian teh :");
+                jmlTeh = input.nextInt();
+                System.out.print("masukkan jumlah pembelian roti :");
+                jmlroti = input.nextInt();
+        
+                totalharga = (jmlKopi*hargakopi) + (jmlTeh*hargaTeh) + (jmlroti*hargaRoti);
+        
+                nominalBayar = totalharga - (diskon*totalharga);
+        
+                System.out.println("Keanggotaan pelanggan " +keanggotaan);
+                System.out.println("item pembelian "+jmlKopi+" kopi "+jmlTeh+" teh "+jmlroti+" roti ");
+                System.out.println("nominal bayar : "+nominalBayar);
+        
+        
+        
+        
+        
+            }
+            
+        }
