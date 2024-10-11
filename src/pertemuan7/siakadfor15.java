ae9fac26173fdbf17f607633fa28fc0ff40769d2
miqi4 <iqbalop098@gmail.com>
2024-10-11 14:50:53 +0700

    P7_Percobaan1_PertanyaanNo5

--- src/pertemuan7/siakadfor15.java
+++ src/pertemuan7/siakadfor15.java
@@ -4,7 +4,8 @@ public class siakadfor15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100, minlulus=60;
+        int jmlhlulus=0 ,jmlhtidaklulus=0 ;
         for (int i = 1; i <= 10; i++) {
             System.out.print("masukkan nilai mahasiswa ke-" +i+" : ");
             nilai = sc.nextDouble();
@@ -14,8 +15,15 @@ public class siakadfor15 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
-            }
+            if (nilai >= minlulus) {
+                jmlhlulus++;
+            } else {
+                jmlhtidaklulus++;    
+            }}
             System.out.println("nilai tertinggi : "+tertinggi);
             System.out.println("nilai terendah  : "+terendah);
-        }
+            System.out.println("Jumlah mahasiswa yang lulus : " + jmlhlulus);
+            System.out.println("Jumlah mahasiswa yang tidak lulus : " + jmlhtidaklulus);
+        
+}
 }
