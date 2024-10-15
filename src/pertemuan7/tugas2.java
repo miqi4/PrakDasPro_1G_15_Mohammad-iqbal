6a2fdeb32f1e9b985b1f7da42c3ec5e8b415fc79
miqi4 <iqbalop098@gmail.com>
2024-10-15 08:00:51 +0700

    P7_Tugas2

--- src/pertemuan7/tugas2.java
+++ src/pertemuan7/tugas2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class tugas2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int jenis,durasi,total=0;
+        int jenis,durasi,totalakhir=0,total=0;
         do{
         System.out.print("masukkan jenis kendaraan (1. mobil 2. motor 0. keluar): ");
         jenis = sc.nextInt();
@@ -11,17 +11,20 @@ public class tugas2 {
             System.out.print("masukkan durasi = ");
             durasi=sc.nextInt();
             if (durasi>5) {
-                total+=12500;
+                System.out.println("total pembayaran = 12500");
+                totalakhir+=12500;
             }
             else if (jenis==1) {
-                total+=durasi*3000;
+                System.out.println("total pembayaran = "+(durasi*3000));
+                totalakhir+=durasi*3000;
             }
             else if (jenis==2) {
-                total+=durasi*2000;
+                System.out.println("total pembayaran = "+(durasi*2000));
+                totalakhir+=durasi*2000;
             }
-            System.out.println("total = "+total); 
+            
         }continue;
     }while(jenis!=0);
-    System.out.print("total akhir = "+total);
+    System.out.print("total pengahasilan = "+totalakhir);
     }
 }
