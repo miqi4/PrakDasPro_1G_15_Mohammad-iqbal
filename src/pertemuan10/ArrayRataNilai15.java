0201a37e66bd69a3770517a6a7cf44834585d00e
miqi4 <iqbalop098@gmail.com>
2024-11-04 20:07:56 +0700

    P10_Percobaan3_PertanyaanNo3

--- src/pertemuan10/ArrayRataNilai15.java
+++ src/pertemuan10/ArrayRataNilai15.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 public class ArrayRataNilai15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaimhs = new int[10];
+        int m=0;
+        System.out.print("masukkan jumlah mahasiswa = ");
+        m = sc.nextInt();
+        int[] nilaimhs = new int[m];
+        int mhs=0;
         double total = 0;
         double rata2 = 0;
         for (int i = 0; i < nilaimhs.length; i++) {
             System.out.print("masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaimhs[i] = sc.nextInt();
             total += nilaimhs[i];
-            
+            if (nilaimhs[i]>70) {
+                mhs++;
+            }
         }rata2 = total/nilaimhs.length;
         System.out.println("rata rata nilai = "+rata2);
+        System.out.println("jumlah mahasiswa lulus = "+mhs);
     }
 }
