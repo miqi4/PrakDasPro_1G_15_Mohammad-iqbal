a536e7a64dd5b0ba603e765d5ff559b976a334d4
miqi4 <iqbalop098@gmail.com>
2024-11-04 18:47:46 +0700

    P10_Percobaan2_PertanyaanNo5

--- src/pertemuan10/arraynilai15.java
+++ src/pertemuan10/arraynilai15.java
@@ -4,11 +4,14 @@ public class arraynilai15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiakhir = new int[10];
-        for (int i = 0 ; i < 10 ; i++) {
+        for (int i = 0 ; i < nilaiakhir.length ; i++) {
             System.out.print("masukkan nilai akhir ke-"+i+" : ");
             nilaiakhir[i] = sc.nextInt();}
-        for (int i = 0; i < 10; i++) {
-            System.out.println("nilai akhir ke-"+i+" adalah "+nilaiakhir[i]);
+        for (int i = 0; i < nilaiakhir.length; i++) {
+            if (nilaiakhir[i]>70) {
+                System.out.println("mahasiswa ke-"+i+" lulus !"); 
+            }
+            else System.out.println("mahasiswa ke-"+i+" tidak lulus !");
         }
     }
 }
