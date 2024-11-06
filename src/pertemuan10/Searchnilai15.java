ac607f42f693e170eae929e6f54b72438ec6a99e
miqi4 <iqbalop098@gmail.com>
2024-11-06 19:16:43 +0700

    P10_Percobaan4_PertanyaanNo4

--- src/pertemuan10/Searchnilai15.java
+++ src/pertemuan10/Searchnilai15.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 public class Searchnilai15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] Arrnilai = {80,85,78,96,90,82,86};
-        int key=90, hasil=0;
-        for (int i = 0; i < Arrnilai.length; i++) {
-            if (key == Arrnilai[i]) {
-                hasil = i;
-                break;
-            }
+        int m=0;
+        System.out.print("masukkan jumlah mahasiswa = ");
+        m = sc.nextInt();
+        int[] Arrnilai = new int[m];
+        int key, hasil=0;
+        for (int j = 0; j < Arrnilai.length; j++) {
+            System.out.print("masukkan nilai mahasiswa ke-"+(j+1)+" : ");
+            Arrnilai[j] = sc.nextInt();}
+            System.out.print("masukkan nilai yang ingin dicari = ");
+            key = sc.nextInt();
+            for (int i = 0; i < Arrnilai.length; i++) {
+                if (key == Arrnilai[i]) {
+                    hasil = i;
+                    System.out.println();
+                    System.out.println("nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+(hasil+1));
+                    System.out.println();break;}
+                if (key != Arrnilai[i] && i==(Arrnilai.length-1)) {
+                    System.out.println();
+                    System.out.println("nilai yang dicari tidak ditemukan");
+                }
         }
-        System.out.println();
-        System.out.println("nilai "+key+" ketemu di indeks ke-"+hasil);
-        System.out.println();
     }
 }
