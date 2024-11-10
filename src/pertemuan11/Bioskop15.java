8777d374932f05e185cbbed976fe2c410d0c6e58
miqi4 <iqbalop098@gmail.com>
2024-11-10 10:40:02 +0700

    P11_Percobaan1

---
+++ src/pertemuan11/Bioskop15.java
@@ -0,0 +1,18 @@
+package pertemuan11;
+
+public class Bioskop15 {
+    public static void main(String[] args) {
+        String[][] Penonnton = new String[4][2];
+        Penonnton[0][0] = "amin";
+        Penonnton[0][1] = "bena";
+        Penonnton[1][0] = "candra";
+        Penonnton[1][1] = "dela";
+        Penonnton[2][0] = "eka";
+        Penonnton[2][1] = "farhan";
+        Penonnton[3][0] = "gisel";
+        System.out.printf("%s \t %s \n",Penonnton[0][0],Penonnton[0][1]);
+        System.out.printf("%s \t %s \n",Penonnton[1][0],Penonnton[1][1]);
+        System.out.printf("%s \t %s \n",Penonnton[2][0],Penonnton[2][1]);
+        System.out.printf("%s \t %s \n",Penonnton[3][0],Penonnton[3][1]);
+    }
+}
