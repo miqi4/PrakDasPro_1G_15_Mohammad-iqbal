a6011d6b7778ccd6add166aacbdef32af9d9c972
miqi4 <iqbalop098@gmail.com>
2024-11-10 19:48:40 +0700

    P11_Percobaan4_PertanyaanNo14

--- src/pertemuan11/Bioskop15.java
+++ src/pertemuan11/Bioskop15.java
@@ -1,18 +1,17 @@
 package pertemuan11;
-
 public class Bioskop15 {
     public static void main(String[] args) {
-        String[][] Penonnton = new String[4][2];
-        Penonnton[0][0] = "amin";
-        Penonnton[0][1] = "bena";
-        Penonnton[1][0] = "candra";
-        Penonnton[1][1] = "dela";
-        Penonnton[2][0] = "eka";
-        Penonnton[2][1] = "farhan";
-        Penonnton[3][0] = "gisel";
-        System.out.printf("%s \t %s \n",Penonnton[0][0],Penonnton[0][1]);
-        System.out.printf("%s \t %s \n",Penonnton[1][0],Penonnton[1][1]);
-        System.out.printf("%s \t %s \n",Penonnton[2][0],Penonnton[2][1]);
-        System.out.printf("%s \t %s \n",Penonnton[3][0],Penonnton[3][1]);
+        String[][] Penonton = new String[4][2];
+        Penonton[0][0] = "amin";
+        Penonton[0][1] = "bena";
+        Penonton[1][0] = "candra";
+        Penonton[1][1] = "dela";
+        Penonton[2][0] = "eka";
+        Penonton[2][1] = "farhan";
+        Penonton[3][0] = "gisel";
+        Penonton[3][1] = "hana";
+        for (int i = 0; i < Penonton.length; i++) {
+            System.out.println("penonton pada baris ke-"+(i+1)+" : "+String.join(" , ",Penonton[i]));
+        }
     }
 }
