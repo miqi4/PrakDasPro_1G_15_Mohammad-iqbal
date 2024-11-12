eb166096976ffad75e49f25942d7bd7a5c1b2102
miqi4 <iqbalop098@gmail.com>
2024-11-12 18:59:49 +0700

    P11_Percobaan3_Pertanyaan

--- src/pertemuan11/numbers15.java
+++ src/pertemuan11/numbers15.java
@@ -8,5 +8,11 @@ public class numbers15 {
         mynumbers[0]= new int[5];
         mynumbers[1]= new int[3];
         mynumbers[2]= new int[1];
+        for (int i = 0; i < mynumbers.length; i++) {
+            System.out.println(Arrays.toString(mynumbers[i]));
+        }
+        for (int i = 0; i < mynumbers.length; i++) {
+            System.out.println("panjang baris ke-"+(i+1)+" : "+mynumbers[i].length);
+        }
     }
 }
