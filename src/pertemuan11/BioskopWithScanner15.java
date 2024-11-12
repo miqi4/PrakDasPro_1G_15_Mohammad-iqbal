1d5a930ade445289fe8f5294b8bc6d2656657af0
miqi4 <iqbalop098@gmail.com>
2024-11-12 10:31:11 +0700

    P11_Percobaan2_PertanyaanNo6

--- src/pertemuan11/BioskopWithScanner15.java
+++ src/pertemuan11/BioskopWithScanner15.java
@@ -9,18 +9,36 @@ public class BioskopWithScanner15 {
         while (true) {
         System.out.print("Masukkan nama: ");
         nama = sc.nextLine();
+        do {
         System.out.print("Masukkan baris: ");
         baris = sc.nextInt();
         System.out.print("Masukkan kolom: ");
         kolom = sc.nextInt();
         sc.nextLine();
-
-        penonton[baris-1][kolom-1] = nama;
+        if (baris>4||kolom>2) {
+            System.out.println("tidak tersedia");
+        continue;}
+        if (penonton[baris - 1][kolom - 1] != null) {
+            System.out.println("Kursi pada baris " + baris + " kolom " + kolom + " sudah terisi. pilih kursi lain.");}
+            else {
+            penonton[baris - 1][kolom - 1] = nama;
+            break;
+        }
+        } while (true);
         System.out.print("Input penonton lainnya? (y/n): ");
         next = sc.nextLine();
         if (next.equalsIgnoreCase("n")) {
         break;
         }
+        }
+        System.out.println();
+        for (int i = 0; i < penonton.length; i++) {
+            for (int j = 0; j < penonton[i].length; j++) {
+                if (penonton[i][j]==null) {
+                    penonton[i][j]="***";
+                }
+                System.out.println(penonton[i][j]+" baris "+(i+1)+" kolom "+(j+1));
+            }
         }
     }
 }
