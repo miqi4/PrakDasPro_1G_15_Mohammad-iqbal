fea54e14f96d62a8a945bbbaa00da1b5edff4fd4
miqi4 <iqbalop098@gmail.com>
2024-11-14 11:19:31 +0700

    P11_Percobaan2_RalatPertanyaan2-5

--- src/pertemuan11/BioskopWithScanner15.java
+++ src/pertemuan11/BioskopWithScanner15.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class BioskopWithScanner15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int baris, kolom;
+        int baris, kolom,menu;
         String nama,next;
         String[][] penonton = new String[4][2];
-        while (true) {
+        do {
+        System.out.println("menu 1 : input data penonton ");
+        System.out.println("menu 2 : tampilkan daftar penonton ");
+        System.out.println("menu 3 : exit ");
+        System.out.print("pilih menu : ");
+        menu = sc.nextInt();
+        sc.nextLine();
+        System.out.println();
+        while (menu==1) {
         System.out.print("Masukkan nama: ");
         nama = sc.nextLine();
         do {
@@ -24,13 +32,15 @@ public class BioskopWithScanner15 {
             penonton[baris - 1][kolom - 1] = nama;
             break;
         }
-        } while (true);
+        } while (menu==1);
+        System.out.println();
         System.out.print("Input penonton lainnya? (y/n): ");
         next = sc.nextLine();
         if (next.equalsIgnoreCase("n")) {
         break;
         }
         }
+        while (menu==2) {
         System.out.println();
         for (int i = 0; i < penonton.length; i++) {
             for (int j = 0; j < penonton[i].length; j++) {
@@ -40,5 +50,11 @@ public class BioskopWithScanner15 {
                 System.out.println(penonton[i][j]+" baris "+(i+1)+" kolom "+(j+1));
             }
         }
+        System.out.println();break;
+        }
+        if (menu==3) {
+            break;
+        }
+    } while (true);
     }
 }
