1995aeafb2604953b20fe7681a727f43f7da2237
miqi4 <iqbalop098@gmail.com>
2024-11-29 14:01:30 +0700

    P13_Percobaan2

--- src/pertemuan13/kafe15.java
+++ src/pertemuan13/kafe15.java
@@ -1,7 +1,11 @@
 package pertemuan13;
 
 public class kafe15 {
-        public static void Menu() {
+        public static void Menu(String namapelanggan , boolean ismember) {
+            System.out.println("selamat datang "+namapelanggan+" !");
+            if (ismember) {
+                System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+            }
             System.out.println("===== MENU RESTO KAFE =====");
             System.out.println("1. Kopi Hitam - Rp 15,000");
             System.out.println("2. Cappuccino - Rp 20,000");
@@ -13,6 +17,6 @@ public class kafe15 {
             System.out.println("Silakan pilih menu yang Anda inginkan.");
             }     
             public static void main(String[] args) {
-                Menu();
+            Menu("andi",true);
             }       
 }
