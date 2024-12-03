2ee6ba9a387f38625e214f8b1a066b4215e6fa73
miqi4 <iqbalop098@gmail.com>
2024-12-03 14:07:49 +0700

    P13_Percobaan3

---
+++ src/pertemuan13/PengunjungCafe15.java
@@ -0,0 +1,13 @@
+package pertemuan13;
+
+public class PengunjungCafe15 {
+    public static void daftarpengunjung(String... namapengunjung) {
+        System.out.println("daftar nama pengunjung");
+        for (int i = 0; i < namapengunjung.length; i++) {
+            System.out.println("- "+namapengunjung[i]);
+        }
+    }
+    public static void main(String[] args) {
+        daftarpengunjung("ali","budi","citra");
+    }
+}
