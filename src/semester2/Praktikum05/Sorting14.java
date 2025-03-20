4a74ddaa4befc0efc64441783272f3713781a068
miqi4 <iqbalop098@gmail.com>
2025-03-20 12:40:19 +0700

    Pertemuan6_percobaan01

---
+++ src/semester2/Praktikum05/Sorting14.java
@@ -0,0 +1,37 @@
+package semester2.Praktikum05;
+public class Sorting14 {
+    int[] data;
+    int jumData;
+    Sorting14(int[] Data, int jmlData){
+        jumData=jmlData;
+        data= new int[jmlData];
+        for (int i = 0; i < jumData; i++) {
+            data[i]=Data[i];}}
+    void bubblesort(){
+        int temp=0;
+        for (int i = 0; i < jumData-1; i++) {
+            for (int j = 1; j < jumData-i; j++) {
+                if (data[j-1]>data[j]) {
+                    temp = data[j];
+                    data[j]=data[j-1];
+                    data[j-1]=temp;}}}}
+    void tampil(){
+        for (int i = 0; i < jumData; i++) {
+            System.out.print(data[i]+" ");}
+            System.out.println();}
+    void selectionsort(){
+        for (int i = 0; i < jumData-1; i++) {
+            int min=i;
+            for (int j=i+1 ; j < jumData; j++) {
+                if (data[j]<data[min]) {min=j;}}
+            int temp=data[i];
+            data[i]=data[min];
+            data[min]=temp;}}
+    void insertionsort(){
+        for (int i = 0; i < jumData; i++) {
+            int temp = data[i];
+            int j=i-1;
+            while (j>=0&&data[j]<temp) {
+                data[j+1]=data[j];
+                j--;}
+            data[j+1]=temp;}}}
---
+++ src/semester2/Praktikum05/SortingMain14.java
@@ -0,0 +1,32 @@
+package semester2.Praktikum05;
+
+public class SortingMain14 {
+    public static void main(String[] args) {
+        int a[]={20,10,2,7,12};
+        int b[]={30,20,2,8,14};
+        int c[]={40,10,4,9,3};
+
+    Sorting14 dataurut1 = new Sorting14(a, a.length);
+    System.out.println("data awal 1");
+    dataurut1.tampil();
+    dataurut1.bubblesort();
+    System.out.println("data sudah diurutkan dengan bubble sort (ASC)");
+    dataurut1.tampil();
+    System.out.println();
+
+    Sorting14 dataurut2 = new Sorting14(b, b.length);
+    System.out.println("data awal 2");
+    dataurut2.tampil();
+    dataurut2.selectionsort();
+    System.out.println("data sudah diurutkan dengan selection sort (ASC)");
+    dataurut2.tampil();
+    System.out.println();
+
+    Sorting14 dataurut3 = new Sorting14(c, c.length);
+    System.out.println("data awal 3");
+    dataurut3.tampil();
+    dataurut3.insertionsort();
+    System.out.println("data sudah diurutkan dengan insertion sort (ASC)");
+    dataurut3.tampil();
+    }
+}
