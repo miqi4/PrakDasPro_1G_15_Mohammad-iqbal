c385b3961caae44701833bdb76c69d830b321023
miqi4 <iqbalop098@gmail.com>
2025-05-22 10:33:24 +0700

    pertemuan13_percobaan02

--- src/semester2/jobsheet12/doubleLinkedList.java
+++ src/semester2/jobsheet12/doubleLinkedList.java
@@ -77,4 +77,55 @@ public class doubleLinkedList {
             System.out.println("Linked list kosong");
         }
     }
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat dihapus!");
+        }else if( size==1){
+            removeLast();
+        }else{
+            head = head.next;
+            head.prev=null;
+            size--;
+        }
+    }
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat dihapus!");
+        }else if (head.next==null) {
+            head=null;
+            size--;
+            return;
+        }
+        Node14 current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai index di luar batas");
+        }else if(index == 0) {
+            removeFirst();
+        } else {
+            Node14 current = head;
+            int i =0;
+            while (i<index) {
+                current=current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            }else if (current.prev==null) {
+                current=current.next;
+                current.prev=null;
+                head=current;
+            }else {
+                current.prev.next = current.next;
+                current.next.prev=current.prev;
+            }
+            size--;
+        }
+    }
 }
--- src/semester2/jobsheet12/doublelinkedlistMain14.java
+++ src/semester2/jobsheet12/doublelinkedlistMain14.java
@@ -3,20 +3,22 @@ public class doublelinkedlistMain14 {
     public static void main(String[] args)throws
     Exception{
         doubleLinkedList dll = new doubleLinkedList();
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
         dll.print();
         System.out.println("Size : "+dll.size());
-        System.out.println("==============================================");
-        dll.addFirst(3);
-        dll.addLast(4);
-        dll.addFirst(7);
+        System.out.println("======================");
+        dll.removeFirst();
         dll.print();
         System.out.println("Size : "+dll.size());
-        System.out.println("==============================================");
-        dll.add(40, 1);
+        System.out.println("======================");
+        dll.removeLast();
         dll.print();
         System.out.println("Size : "+dll.size());
-        System.out.println("==============================================");
-        dll.clear();
+        System.out.println("======================");
+        dll.remove(1);
         dll.print();
         System.out.println("Size : "+dll.size());
     }
